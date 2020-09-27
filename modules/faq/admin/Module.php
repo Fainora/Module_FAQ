f49f01e074d217857d67ef22ae240ae50a750a8f
Fainora <fainora24@mail.ru>
2020-09-27 19:15:39 +0400

    sugn up/out

--- modules/faq/admin/Module.php
+++ modules/faq/admin/Module.php
@@ -2,8 +2,12 @@
 
 namespace app\modules\faq\admin;
 
+use Yii;
+use yii\filters\AccessControl;
+
 class Module extends \yii\base\Module
 {
+    public $layout = '/admin';
 
     public $controllerNamespace = 'app\modules\faq\admin\controllers';
 
@@ -12,5 +16,27 @@ class Module extends \yii\base\Module
         parent::init();
 
         // custom initialization code goes here
+    }
+
+    public function behaviors()
+    {
+        return [
+            'access'    =>  [
+                'class' =>  AccessControl::className(),
+                'denyCallback'  =>  function($rule, $action)
+                {
+                    throw new \yii\web\NotFoundHttpException();
+                },
+                'rules' =>  [
+                    [
+                        'allow' =>  true,
+                        'matchCallback' =>  function($rule, $action)
+                        {
+                            return Yii::$app->user->identity->isAdmin;
+                        }
+                    ]
+                ]
+            ]
+        ];
     }
 }
